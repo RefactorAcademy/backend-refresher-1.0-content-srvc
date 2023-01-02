8119f17db6321ccd70215de084639675476d19f0
verne1997 <sourjya@refactor.academy>
2023-01-02 11:33:38 +0530

    added rmq connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ["amqps://uulucdbz:n6vg2Oub_EdrHrYY9rtP2bwfh5CkDAj2@shark.rmq.cloudamqp.com/uulucdbz"],
+      queue: "CONTENT_SRVC_QUEUE",
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+  
+  await app.startAllMicroservices();
+  
+  await app.listen(5000);
 }
 bootstrap();
