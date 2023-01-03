5ce7edb384dc7f6722eda7fe74ad4d30fb0584a1
verne1997 <sourjya@refactor.academy>
2023-01-03 11:38:21 +0530

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { queues } from './submodules/backend-refresher-1.0-rmq/src/constants/rmqQueues';
+import { MsgBrokerOpsService } from './submodules/backend-refresher-1.0-rmq/src/module/msg-broker-ops/msg-broker-ops.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name:'CONTENT_SERVICE_QUEUE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ["amqps://uulucdbz:n6vg2Oub_EdrHrYY9rtP2bwfh5CkDAj2@shark.rmq.cloudamqp.com/uulucdbz"],
+          queue: queues.CONTENT_SERVICE_QUEUE,
+          queueOptions: {
+            durable: true,
+          }
+        
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,MsgBrokerOpsService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { queues } from './submodules/backend-refresher-1.0-rmq/src/constants/rmqQueues';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,7 @@ async function bootstrap() {
     transport: Transport.RMQ,
     options: {
       urls: ["amqps://uulucdbz:n6vg2Oub_EdrHrYY9rtP2bwfh5CkDAj2@shark.rmq.cloudamqp.com/uulucdbz"],
-      queue: "CONTENT_SRVC_QUEUE",
+      queue: queues.CONTENT_SERVICE_QUEUE,
       queueOptions: {
         durable: true,
       },
